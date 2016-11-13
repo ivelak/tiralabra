8634aa16d52d8eaf982851cdbde9765683e0f571
Antti Rantapelkonen <rantaana@gmail.com>
2016-11-13 20:07:07 +0200

    JUnit testit luokalle InsertionSort

---
+++ sortof/test/sortof/InsertionSortTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortof;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author antti
+ */
+public class InsertionSortTest {
+
+    public InsertionSort sorter;
+    public int[] empty;
+    public int[] oneElement;
+    public int[] ordered;
+    public int[] unordered;
+
+    public InsertionSortTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+
+        this.empty = new int[]{};
+        this.oneElement = new int[]{1};
+        this.ordered = new int[]{-5, -2, 0, 2, 5};
+        this.unordered = new int[]{5, 2, -5, -2, 0};
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * InsertionSort -luokan sort-metodin testit.
+     */
+    @Test
+    public void worksWithEmpty() {
+        sorter = new InsertionSort(empty);
+        int[] sortedEmpty= sorter.sort();
+        
+        assertArrayEquals(new int[]{}, sortedEmpty);
+    }
+    
+    @Test
+    public void worksWithOneElement(){
+        sorter = new InsertionSort(oneElement);
+        int[] sortedOneElement= sorter.sort();
+        
+        assertArrayEquals(new int[]{1}, sortedOneElement);
+    }
+    
+    @Test
+    public void worksWithOrdered(){
+        sorter = new InsertionSort(ordered);
+        int[] sortedInOrder= sorter.sort();
+        
+        assertArrayEquals(new int[]{-5, -2, 0, 2, 5}, sortedInOrder);
+    }
+    
+    @Test
+    public void sortsUnorderedList(){
+        sorter = new InsertionSort(unordered);
+        int[] sorted= sorter.sort();
+        
+        assertArrayEquals(new int[]{-5, -2, 0, 2, 5}, sorted);
+    }
+
+}
